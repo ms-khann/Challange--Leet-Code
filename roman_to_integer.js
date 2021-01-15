31fdfb24259a7141b62ad34fe33bb3ccec48a5f1
ms-khann <ms.solo786@gmail.com>
2021-01-15 21:49:17 +0530

    update

--- roman_to_integer.js
+++ roman_to_integer.js
@@ -1,15 +1,48 @@
-let s = "IIVG";
-var romanToInt = function(s) {
+let s = "MCMXCIV";
+var romanToInt = function (s) {
     let roman = [...s.toUpperCase()];
-    let valid_char = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-    let i = 0;
-    roman.some(function(current){
-        if( valid_char.includes(current) == false ){
-            console.log(i++);
+    let valid_char = ["I", "V", "X", "L", "C", "D", "M"];
+    let num_arr = [];
+    let result = 0;
+    for (let i = 0; i < roman.length; i++) {
+        if (valid_char.includes(roman[i]) == false) {
             return false;
         }
-    });
-    // console.log(is_valid);
-    return roman;
+        let current_char = roman[i];
+        switch (current_char) {
+            case "I":
+                num_arr.push(1);
+                break;
+            case "V":
+                num_arr.push(5);
+                break;
+            case "X":
+                num_arr.push(10);
+                break;
+            case "L":
+                num_arr.push(50);
+                break;
+            case "C":
+                num_arr.push(100);
+                break;
+            case "D":
+                num_arr.push(500);
+                break;
+            case "M":
+                num_arr.push(1000);
+                break;
+        }
+    }
+    // 1,1,1
+    for(let i=0;i<num_arr.length;i++){
+        if(num_arr[i] <= result){
+            result += num_arr[i];
+        }
+        else{
+            result = num_arr[i] -result;
+        }
+        console.log(result);
+    }
+    return result;
 };
 console.log(romanToInt(s));
